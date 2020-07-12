19bceff2637d79b275343020558d728f284ed990
devtinatina <53737471+devtinatina@users.noreply.github.com>
2020-07-13 05:04:17 +0900

    Updates

---
+++ precache-manifest.cdd9180d947412644e4f644443eb776b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d0293f3d427469eab941aab56ae471",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "20282c6c1e8ecd30bac9",
+    "url": "/movie_app_react/static/css/main.9f4c8c8d.chunk.css"
+  },
+  {
+    "revision": "6f624be920fcd598d4f8",
+    "url": "/movie_app_react/static/js/2.a787a7af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_react/static/js/2.a787a7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20282c6c1e8ecd30bac9",
+    "url": "/movie_app_react/static/js/main.451d01f5.chunk.js"
+  },
+  {
+    "revision": "e772194c85f13f1bd6a9",
+    "url": "/movie_app_react/static/js/runtime-main.b1700b33.js"
+  }
+]);
